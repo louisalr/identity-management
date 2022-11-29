bdaa570cf3fbbe840f82163d96ddce2668bdce40
Louis Alary <louis.alary34@gmail.com>
2022-11-29 12:03:27 +0100

    Created ldap-list and page-not-found components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LdapListComponent } from './ldap-list/ldap-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LdapListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
