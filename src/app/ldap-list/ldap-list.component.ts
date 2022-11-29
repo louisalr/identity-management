b87d27b0583cd6e80bf7f081f111f225094d5c3a
Louis Alary <louis.alary34@gmail.com>
2022-11-29 14:25:54 +0100

    Render ldap users from modem

--- src/app/ldap-list/ldap-list.component.ts
+++ src/app/ldap-list/ldap-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
+import {UserLdap} from "../model/user-ldap";
+import {MatPaginator} from "@angular/material/paginator";
+import {LDAP_USERS} from "../model/ldap-mock-data";
 
 @Component({
   selector: 'app-ldap-list',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LdapListComponent implements OnInit {
 
+  displayedColumns: string [] = ['nomComplet', 'mail', 'employeNumero'];
+  dataSource = new MatTableDataSource<UserLdap>([])
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator
+
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.getUsers();
+  }
+
+  private getUsers(): void{
+    this.dataSource.data = LDAP_USERS;
   }
 
 }
