39b688815ab9f1563d22962f75c6c08395fc91f8
Louis Alary <louis.alary34@gmail.com>
2022-12-02 16:30:04 +0100

    User and password validators

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {UserLdap} from "../model/user-ldap";
 import {LDAP_USERS} from "../model/ldap-mock-data";
-import {Observable, of} from "rxjs";
+import {Observable, of, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,7 @@ import {Observable, of} from "rxjs";
 export class UsersService {
 
   users: UserLdap[] = LDAP_USERS;
+  private static users: UserLdap[]; //for the observable
 
   constructor() { }
 
@@ -18,5 +19,24 @@ export class UsersService {
 
   getUser(login: string): Observable<UserLdap>{
     return of (this.users.find(user => user.login === login));
+  }
+
+  addUser(user: UserLdap): Observable<UserLdap> {
+    UsersService.users.push(user);
+    return of(user);
+  }
+
+  updateUser(userToUpdate: UserLdap): Observable<UserLdap>{
+    const user = UsersService.users.find( u => u.login === userToUpdate.login);
+    if (user){
+      user.nom = userToUpdate.nom;
+      user.prenom = userToUpdate.prenom;
+      user.nomComplet = user.nom + ' ' + user.prenom;
+      user.motDePasse = userToUpdate.motDePasse
+
+      return of(userToUpdate)
+    }
+
+    return throwError('Utilisateur non trouvé');
   }
 }
