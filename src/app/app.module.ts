99abe99a249f5012aac4bc53921b5e8d15550741
Louis Alary <louis.alary34@gmail.com>
2023-01-21 15:55:08 +0100

    Login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LdapListComponent } from './ldap-management/ldap-list/ldap-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app-material.module';
@@ -14,46 +13,45 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import {MatTableModule} from "@angular/material/table";
-import {MatPaginatorModule} from "@angular/material/paginator";
-import {MatInputModule} from "@angular/material/input";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { LdapEditComponent } from './ldap-management/ldap-edit/ldap-edit.component';
-import { LdapAddComponent } from './ldap-management/ldap-add/ldap-add.component';
-import { AlertComponent } from './share/alert/alert.component';
 import { LdapManagementModule } from './ldap-management/ldap-management.module';
 import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
 import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryUsersService} from "./service/in-memory-users.service";
+import { LoginComponent } from './security/login/login.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     NavbarComponent,
+    LoginComponent,
   ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        ReactiveFormsModule,
-        LdapManagementModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        AppMaterialModule,
-        LayoutModule,
-        MatSidenavModule,
-        MatSnackBarModule,
-        MatIconModule,
-        MatToolbarModule,
-        MatListModule,
-        MatButtonModule,
-        HttpClientModule,
-        HttpClientInMemoryWebApiModule.forRoot(
-          InMemoryUsersService, {dataEncapsulation: false}
-        )
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LdapManagementModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+    LayoutModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryUsersService, {dataEncapsulation: false}
+    ),
+    MatInputModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
