ed2d40850c50d7818af4ea438099c1ea9f9c6a6b
Louis Alary <louis.alary34@gmail.com>
2023-01-21 15:56:02 +0100

    Apply new routing system with Authguard

--- src/app/ldap-management/ldap-management-routing.module.ts
+++ src/app/ldap-management/ldap-management-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import {LdapListComponent} from "./ldap-list/ldap-list.component";
 import {LdapAddComponent} from "./ldap-add/ldap-add.component";
 import {LdapEditComponent} from "./ldap-edit/ldap-edit.component";
+import {AuthGuard} from "../security/auth.guard";
 
-const routes: Routes = [
-  { path: 'users/list', component: LdapListComponent},
-  { path: 'user/add', component: LdapAddComponent},
-  { path: 'user/:id', component: LdapEditComponent},
-  { path: '', redirectTo: 'users/list', pathMatch: 'full'},
+const routes: Routes = [{
+  path: 'users',
+  component: LdapListComponent,
+  canActivate: [AuthGuard],
+  children: [
+    { path: 'list', component: LdapListComponent},
+    { path: 'add', component: LdapAddComponent},
+    { path: ':id', component: LdapEditComponent},
+    { path: '', redirectTo: 'users/list', pathMatch: 'full'},
+  ]}
 ];
 
 @NgModule({
